78cf526169b31f01cf8d0b159e68a5f1217c9dd1
Rostyslavderii <105818935+Rostyslavderii@users.noreply.github.com>
2022-10-31 11:00:31 +0200

    go

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,31 @@ import { Section } from './Section/Section';
 import { FormAddContact } from './FormAddContact/FormAddContact';
 import { ListContacts } from './ListContacts/ListContacts';
 import { Input } from './Input/Input';
+// import { createRef } from 'react';
+// import { postsRequest } from './services/api';
+
+const LS_KEY = 'Contacts';
+const contactsFromLS = localStorage.getItem(LS_KEY);
+const parsedContactsFromLS = JSON.parse(contactsFromLS);
+
+const contactsFromStart = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: parsedContactsFromLS ? parsedContactsFromLS : contactsFromStart,
     filter: '',
-    name: '',
-    number: '',
   };
+
+  // page: "1",
+  // posts: [],
+  // isLoading: false,
+  // error:'',
+  //};
 
   // onLeaveFeedback = e => {
   //   this.setState(prevState => ({ [key]: prevState[key] + 1 }));
@@ -24,6 +36,8 @@ class App extends Component {
   //   // this.countPositiveFeedbackPercentage();
   //   //
   // };
+
+  // bntRef = createRef();
 
   deleteContact = e => {
     this.setState(prevState => ({
@@ -50,11 +64,78 @@ class App extends Component {
         ...prevState.contacts,
       ] /* (data +good:1, good:2, good:3,) */,
     }));
+
+    const overlap = this.state.contacts.filter(
+      contact =>
+        contact.name.toLowerCase() === newContact.name.toLowerCase() &&
+        contact.number.toLowerCase() === newContact.number.toLowerCase()
+    );
+    overlap.length === 0
+      ? this.setState(prevState => ({
+          contacts: [...prevState.contacts, newContact],
+        }))
+      : alert('This contact has been added');
   };
 
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
+
+  // onNextPage = () => {
+  //   this.setState(prevState => ({
+  //     page: prevState.page + 1,
+  //   }));
+  // }
+
+  // handleResizeScreen = e => (
+  //   console.log(window.innerWidth)
+  // )
+
+  // componentDidMount() {
+  //   // window.addEventListener('resize', () => this.handleInput(this.state.props))
+
+  //   // fetch("https:/")
+  //   //   .then(respone => Response.json())
+  //   //   .then(data => this.setState({ posts: data }));
+
+  //   this.fetchPosts(); //!!
+
+  // }
+
+  // fetchPosts = async () => {
+  //   try { // встановлюємо іникатор завантаження та обновлюемо помилку
+  //     this.setState({ error: 'Bad Request', isLoading: true });
+  //     const postsData = await postsRequest();    // const response = await fetch('htttps')
+  //     // const data = await response.json();
+
+  //     this.setState({ posts: data }); // або !!!
+  //     this.setState(prevState => ({
+  //       posts: [...prevState.post, ...postsData],
+  //     }))
+  //   } catch (e) {
+  //     this.setState({ error: err.message });
+  //   } finally {
+  //     this.setState({ isLoading: false });
+  //   }
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.page !== this.state.page ||
+  //     prevState.todos !== this.state.todos) {
+  //     console.log(`${this.state.page}`);
+  //     this.fetchPosts(this.state.page)
+  //   }
+  // }
+
   render() {
+    // console.log(this.btnRef.current);
+    // const {posts, error, isLoading} = this.state; //!!!
     return (
       <>
+        {/* {error && <Error className="error">Some error occured: {error}</Error>}
+        {isLoading && <Loader className="loader">loading</Loader>} */}
         <Section title="Phonebook">
           <FormAddContact
             addNewContact={this.addNewContact}
@@ -69,39 +150,13 @@ class App extends Component {
             userDelete={this.deleteContact}
           />
         </Section>
+        {/* <button ref={this.btnRef} className="btn" onChange={this.onNextPage}>
+          {' '}
+          NextPage
+        </button> */}
       </>
     );
   }
 }
 
 export { App };
-
-// handleIncrementgood = e => {
-//   // if (this.state.count >= 100) return;
-//   this.setState(prevState => {
-//     console.log(e.currentTarget);
-//     return {
-//       good: prevState.good + 1,
-//     };
-//   });
-//   this.countTotalFeedback();
-//   this.countPositiveFeedbackPercentage();
-// };
-
-// countTotalFeedback = e => {
-//   this.setState(prevState => {
-//     console.log(e.currentTarget);
-//     return {
-//       total: prevState.good + prevState.neutral + prevState.bad,
-//     };
-//   });
-// };
-
-// countPositiveFeedbackPercentage = e => {
-//   this.setState(prevState => {
-//     console.log(e.currentTarget);
-//     return {
-//       feedback: Math.round((prevState.good * 100) / prevState.total),
-//     };
-//   });
-// };
